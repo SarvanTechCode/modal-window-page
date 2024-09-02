817d5c5ce57827b791632d5ca729733880736c47
SarvanTechCode <sarvan@sarvans-MacBook-Air.local>
2024-09-02 19:45:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modeldata = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const closeModel = document.querySelector('.close-modal');
+const showModel = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModel.length; i++)
+  console.log(showModel[i].textContent);
