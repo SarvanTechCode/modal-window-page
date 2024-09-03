df9c9f97e4101b112042f1c4ee9ea94103040757
SarvanTechCode <sarvan@sarvans-MacBook-Air.local>
2024-09-03 21:12:22 +0530

    added functionality if we click buttons

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 'use strict';
-
-const modeldata = document.querySelector('.model');
+//learn classList
+const modeldata = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModel = document.querySelector('.close-modal');
 const showModel = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < showModel.length; i++)
-  console.log(showModel[i].textContent);
+const openView = function () {
+  modeldata.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeView = function () {
+  modeldata.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const closesideclick = function () {
+  modeldata.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < showModel.length; i++) {
+  console.log('button clicked');
+  showModel[i].addEventListener('click', openView);
+  closeModel.addEventListener('click', closeView);
+  overlay.addEventListener('click', closesideclick);
+}
